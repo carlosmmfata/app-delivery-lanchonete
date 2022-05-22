a39716c12e80b0c8f262c73d737be1c32f1f65c5
User <carlosmmfata@gmail.com>
2022-05-22 17:02:23 -0300

    first commit

---
+++ admin-cardapio-digital-master/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCCtixUf44hyGqCQHNI5z0iIt859F17eqo",
+    authDomain: "appdelivery-cardapio-digital.firebaseapp.com",
+    projectId: "appdelivery-cardapio-digital",
+    storageBucket: "appdelivery-cardapio-digital.appspot.com",
+    messagingSenderId: "300695200677",
+    appId: "1:300695200677:web:ecc3b12f1b1701fa9d3e4c"
+
+  }
+ };
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
---
+++ app-cardapio-digital-master/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCCtixUf44hyGqCQHNI5z0iIt859F17eqo",
+    authDomain: "appdelivery-cardapio-digital.firebaseapp.com",
+    projectId: "appdelivery-cardapio-digital",
+    storageBucket: "appdelivery-cardapio-digital.appspot.com",
+    messagingSenderId: "300695200677",
+    appId: "1:300695200677:web:ecc3b12f1b1701fa9d3e4c"
+  }
+ };
